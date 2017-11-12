5c2eabf60075716780f56d29881ab579d0e47dfa
gdangelo <gregory@gdangelo.fr>
2017-11-12 16:35:58 +0100

    feat: Extract HOG features from image

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ import matplotlib.image as mpimg
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
+from skimage.feature import hog
 
 # Define a function to compute binned color features
-def bin_spatial(img, size=(32, 32)):
+def bin_spatial(img, size=32):
     # Resize the image and use ravel() to flat the array
-    features = cv2.resize(img, size).ravel()
+    features = cv2.resize(img, (size,size)).ravel()
     return features
 
 # Define a function to compute color histogram features
@@ -25,13 +26,23 @@ def color_hist(img, nbins=32, bins_range=(0, 256)):
     features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
     return features
 
+# Define a function to return HOG features and visualization
+def get_hog_features(img, orient=9, pixels_per_cell=(8,8), cells_per_block=(2,2), vis=False, feature_vec=False):
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pixels_per_cell,pixels_per_cell), cells_per_block=(cells_per_block,cells_per_block), transform_sqrt=True, visualise=vis, feature_vector=feature_vec, block_norm='L2-Hys')
+        return features, hog_image
+    else:
+        features = hog(img, orientations=orient, pixels_per_cell=(pixels_per_cell,pixels_per_cell), cells_per_block=(cells_per_block,cells_per_block), transform_sqrt=True, visualise=vis, feature_vector=feature_vec, block_norm='L2-Hys')
+        return features
+
 # Define a function to extract features from a list of images
 # Have this function call bin_spatial() and color_hist()
-def extract_features(imgs, cspace='RGB', spatial_size=(32, 32), hist_bins=32, hist_range=(0, 256)):
+def extract_features(imgs, cspace='RGB', spatial_size=32, hist_bins=32, hist_range=(0, 256), orient=9, pixels_per_cell=8, cells_per_block=2, hog_channel=0):
     # Create a list to append feature vectors to
     features = []
     # Iterate through the list of images
     for file in imgs:
+        file_features = []
         # Read in each one by one
         img = mpimg.imread(file)
         # Apply color conversion if other than 'RGB'
@@ -43,24 +54,41 @@ def extract_features(imgs, cspace='RGB', spatial_size=(32, 32), hist_bins=32, hi
             feature_img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
         elif cspace == 'LUV':
             feature_img = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+        elif cspace == 'YCrCb':
+            feature_img = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
         else:
             feature_img = np.copy(img)
         # Apply bin_spatial() to get spatial color features
         spatial_features = bin_spatial(feature_img, size=spatial_size)
+        file_features.append(spatial_features)
         # Apply color_hist() also with a color space option now
         hist_features = color_hist(img, nbins=hist_bins, bins_range=hist_range)
+        file_features.append(hist_features)
+        # Call get_hog_features() with vis=False, feature_vec=True
+        if hog_channel == -1: # means all channels
+            hog_features = []
+            for channel in range(img.shape[-1]):
+                hog_features.append(get_hog_features(img[:,:,channel], orient, pixels_per_cell, cells_per_block, vis=False, feature_vec=True))
+            hog_features = np.ravel(hog_features)
+        else:
+            hog_features = get_hog_features(img[:,:,hog_channel], orient, pixels_per_cell, cells_per_block, vis=False, feature_vec=True)
+        file_features.append(hog_features)
         # Append the new feature vector to the features list
-        features.append(np.concatenate((spatial_features, hist_features)))
+        features.append(np.concatenate(file_features))
     # Return list of feature vectors
     return features
 
 if __name__ == '__main__':
     # Parse command line arguments
     cspace = 'RGB'
-    spatial_size=(32,32)
+    spatial_size=32
     hist_bins=32
+    orient=9
+    pixels_per_cell=8
+    cells_per_block=2
+    hog_channel=0
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "", ["cspace=", "spatial_size=", "hist_bins="])
+        opts, args = getopt.getopt(sys.argv[1:], "", ["cspace=", "spatial_size=", "hist_bins=", "orient=", "pixels_per_cell=", "cells_per_block=", "hog_channel="])
     except getopt.GetoptError:
         print('main.py --cspace <cspace> --spatial_size <spatial_size> --hist_bins <hist_bins>')
         sys.exit(2)
@@ -71,9 +99,17 @@ if __name__ == '__main__':
         elif opt in ("--cspace"):
             cspace = arg
         elif opt in ("--spatial_size"):
-            spatial_size = arg
+            spatial_size = int(arg)
         elif opt in ("--hist_bins"):
-            hist_bins = arg
+            hist_bins = int(arg)
+        elif opt in ("--orient"):
+            orient = int(arg)
+        elif opt in ("--pixels_per_cell"):
+            pixels_per_cell = int(arg)
+        elif opt in ("--cells_per_block"):
+            cells_per_block = int(arg)
+        elif opt in ("--hog_channel"):
+            hog_channel = int(arg)
 
     # Read in car and non-car images
     vehicles = []
@@ -84,9 +120,9 @@ if __name__ == '__main__':
         non_vehicles.append(file)
 
     # Extract features from vehicles and non vehicles dataset
-    print("Extract features from datasets with:\n- cspace={}\n- spatial_size={}\n- hist_bins={}".format(cspace, spatial_size, hist_bins))
-    vehicles_features = extract_features(vehicles, cspace=cspace, spatial_size=spatial_size, hist_bins=hist_bins)
-    non_vehicles_features = extract_features(non_vehicles, cspace=cspace, spatial_size=spatial_size, hist_bins=hist_bins)
+    print("Extract features from datasets with:\n- cspace={}\n- spatial_size={}\n- hist_bins={}\n- orient={}\n- pixels_per_cell={}\n- cells_per_block={}\n- hog_channel={}".format(cspace, spatial_size, hist_bins, orient, pixels_per_cell, cells_per_block, hog_channel))
+    vehicles_features = extract_features(vehicles, cspace=cspace, spatial_size=spatial_size, hist_bins=hist_bins, orient=orient, pixels_per_cell=pixels_per_cell, cells_per_block=cells_per_block, hog_channel=hog_channel)
+    non_vehicles_features = extract_features(non_vehicles, cspace=cspace, spatial_size=spatial_size, hist_bins=hist_bins, orient=orient, pixels_per_cell=pixels_per_cell, cells_per_block=cells_per_block, hog_channel=hog_channel)
 
     # Create an array stack of feature vectors
     X = np.vstack((vehicles_features, non_vehicles_features)).astype(np.float64)
